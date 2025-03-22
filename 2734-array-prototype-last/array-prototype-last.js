50d4467b47a6fd628a1ae5fa47579f3d47fd6ef6
Sayandeep Pal <145487074+Sayandeep-Pal@users.noreply.github.com>
2025-03-22 11:23:02 +0530

    Time: 35 ms (92.32%) | Memory: 53.1 MB (37.44%) - LeetSync

---
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -0,0 +1,14 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(this.length == 0) return -1;
+
+    this.reverse();
+    return this[0];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
